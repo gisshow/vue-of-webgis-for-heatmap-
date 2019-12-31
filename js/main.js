24b4de690a5f827909bcba128769487814f73c94
lifenhua1232456 <li_fenhua@126.com>
2019-12-31 11:40:01 +0800

    Signed-off-by: lifenhua1232456 <li_fenhua@126.com>

---
+++ js/main.js
@@ -0,0 +1,65 @@
+﻿dojo.require("esri.map");
+dojo.require("esri.layers.ArcGISTiledMapServiceLayer");
+dojo.require("esri.layers.ArcGISDynamicMapServiceLayer");
+dojo.require("src/Echarts3Layer");
+
+dojo.ready(function () {
+
+    var map = new esri.Map('map', 'ArcGISTiledMapServiceLayer', {
+
+        center: [124354.03889350894, 22297.06504766915],
+        zoom: 11,
+        navigationMode: "css-transform",
+        force3DTransforms: true,
+        logo: false,
+        fitExtent: true,
+        fadeOnZoom: true,
+        slider: false
+    });
+
+    var tiled = new esri.layers.ArcGISTiledMapServiceLayer("http://web.zjj.sz.gov.cn/gis/szgeo/szmap_gov");
+    map.addLayer(tiled);
+
+    // var demographicsLayer1 = new esri.layers.ArcGISDynamicMapServiceLayer("http://desktop-v1i9rte:6080/arcgis/rest/services/MyMapService88/MapServer");
+    // map.addLayer(demographicsLayer1);
+
+
+    dojo.connect(map, 'onClick', function (themap) {
+        // console.log(themap.mapPoint);
+        //  console.log("themap",map.getLevel())
+    });
+
+
+    dojo.connect(map, 'onZoom', function (themap) {
+         console.log("themap",map.getLevel())
+
+    })
+    dojo.connect(map, 'onLoad', function (themap) {
+
+
+
+        var heatmap = Dcq.Heatmap.Instance();
+        if (!heatmap.isInited()) {
+            heatmap.init({
+                'map': map,
+                'echarts': echarts
+            });
+        }
+        window.heatmap = heatmap
+        window.echarts = echarts
+
+        //开始获取数据
+        $.get("data.json", function (data) {
+            var result = [];
+            for (var i = 0; i < data.length; i++) {
+                for (var j = 0; j < data[i].length; j++) {
+                    var item = data[i][j];
+                    result.push( [item.coord[0], item.coord[1]].concat(Math.ceil(Math.random()*100))  );
+                }
+            }
+            console.log("result",result)
+           // heatmap.drawpoints(result);
+            heatmap.drawChart(result);
+        }, "json")
+    });
+});
---
+++ js/main1.js
@@ -0,0 +1,49 @@
+﻿dojo.require("esri.map");
+dojo.require("esri.layers.ArcGISTiledMapServiceLayer");
+dojo.require("esri.layers.ArcGISDynamicMapServiceLayer");
+dojo.require("src/Echarts3Layer");
+
+dojo.ready(function() {
+
+	var map = new esri.Map('map','ArcGISTiledMapServiceLayer', {
+ 
+		center: [124354.03889350894,22297.06504766915],
+		zoom: 11,
+		navigationMode: "css-transform",
+		force3DTransforms: true,
+		logo: false,
+		fitExtent: true,
+		fadeOnZoom: true,
+		slider: false
+	});
+
+   var tiled = new esri.layers.ArcGISTiledMapServiceLayer("http://web.zjj.sz.gov.cn/gis/szgeo/szmap_gov");
+   map.addLayer(tiled);
+
+   	dojo.connect(map, 'onClick', function(themap) {
+          console.log(themap.mapPoint);
+	});
+
+	dojo.connect(map, 'onLoad', function(themap) {
+
+		var heatmap = Dcq.Heatmap.Instance();
+		if(!heatmap.isInited()) {
+			heatmap.init({
+				'map': themap,
+				'echarts': echarts
+			});
+		}
+
+		//开始获取数据
+		$.get("data.json", function(data) {
+			var result = [];
+			for(var i = 0; i < data.length; i++) {
+				for(var j = 0; j < data[i].length; j++) {
+					var item = data[i][j];
+					result.push([item.coord[0], item.coord[1]]);
+				}
+			}
+			heatmap.drawChart(result);
+		}, "json");
+	});
+});
